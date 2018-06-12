cd43b92fc093b0a4917bd8383020690b379089b0
chagab <gabriel.chatelain@googlemail.com>
2018-06-12 13:09:29 +0200

    add www/ to Web/
    added server backend to the Web file

---
+++ Web/www/index.js
@@ -0,0 +1,31 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+// Get port from environment and store in Express.
+const port = process.env.PORT || '8000';
+// Create HTTP server.
+const server = http.createServer(app);
+
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.set('port', port);
+
+// Listen on provided port, on all network interfaces.
+server.listen(port, () => console.log(`API running on localhost:${port}`));
---
+++ Web/www/storeTemperature.js
@@ -0,0 +1,99 @@
+const redis = require('redis');
+const Cylon = require("cylon");
+const port = 6379;
+const url = '127.0.0.1';
+const numberOfSensor = 6;
+// TODO: implement a secure connection
+// const tls = require('tls');
+// const fs = require('fs');
+
+// for the current date
+// d is a date object
+function today(d) {
+	return ((d.getDate() < 10) ? "0" : "") + d.getDate() + "/" + (((d.getMonth() + 1) < 10) ? "0" : "") + (d.getMonth() + 1) + "/" + d.getFullYear();
+}
+
+// for the current time
+// d is a date object
+function timeNow(d) {
+	return ((d.getHours() < 10) ? "0" : "") + d.getHours() + ":" + ((d.getMinutes() < 10) ? "0" : "") + d.getMinutes() + ":" + ((d.getSeconds() < 10) ? "0" : "") + d.getSeconds();
+}
+
+
+const client = redis.createClient(port, url, {
+	no_ready_check: true
+});
+
+// client.auth(null, function(err) {
+//      if (err) throw err;
+// });
+
+
+// const ssl = {
+//   key: fs.readFileSync('path_to_keyfile',encoding='ascii'),
+//   cert: fs.readFileSync('path_to_certfile',encoding='ascii'),
+//   ca: [ fs.readFileSync('path_to_ca_certfile',encoding='ascii') ]
+// };
+
+client.on('connect', (err) => {
+	if (err) {
+		throw err;
+	} else {
+		console.log('Connected to Redis');
+		Cylon.robot({
+			connections: {
+				arduino: {
+					adaptor: "firmata",
+					port: "/dev/ttyACM0"
+				}
+			},
+			// For this work we are using TMP36 sensor
+			// initialize all the used pins
+			devices: (function() {
+				// we create an empty object ...
+				let res = {};
+				// to which we add every pin object
+				for (let i = 0; i < numberOfSensor; i++) {
+					res[`a${i}`] = {
+						driver: "analogSensor",
+						pin: i
+					}
+				}
+				return res;
+			})(),
+
+			// then we declare the work to be done
+			work: function(my) {
+				// every seconde ...
+				every((1).second(), function() {
+					// we create an empty array that will contain the temperature data
+					let temperatures = [];
+					// for each pin ...
+					for (let pin = 0; pin < numberOfSensor; pin++) {
+						// we get the analog value that is read
+						const analogValue = my[`a${pin}`].analogRead();
+						// convert it to a voltage
+						const voltage = (analogValue * 5.0) / 1024;
+						// convert this voltage to a temperature and store in the array
+						const temperature = (voltage - 0.5) * 100;
+						// the computed temperature is pushed to the temperatures array
+						temperatures.push(temperature);
+						// we insert the value into the database
+						const d = new Date();
+						const key = `${today(d)}-${timeNow(d)}-A${pin}`;
+						client.set(key, temperature, redis.print);
+					}
+					console.log(temperatures);
+				});
+			}
+		}).start();
+	}
+});
+
+client.on('disconnect', (err) => {
+	if (err) {
+		throw err;
+	} else {
+		console.log('Disconnect from Redis');
+	}
+});
